e232a0e8c8856f2dfc36ec359fad1aed3e360ab5
titopotito <jdf.tito@gmail.com>
2024-02-29 12:55:53 +0800

    Refactored and optimized code for walking

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,19 +10,29 @@ export default class Game extends Phaser.Scene {
     create() {
         this.map = this.make.tilemap({ key: "TilesetFloor" });
         const tileset = this.map.addTilesetImage("TilesetFloor", "tileset-floor");
-
         this.map.createLayer("ground", tileset);
-
-        const character = this.add.sprite(360 / 2, 240 / 2, "character", "walk-down-4.png");
-
-        this.anims.create({
-            key: "idle",
-            frames: [{ key: "character", frame: "idle-right-1.png" }],
-        });
+        this.character = this.physics.add.sprite(360 / 2, 240 / 2, "character", "idle-down-1.png");
 
         this.createAnimation("character", "idle", 1);
         this.createAnimation("character", "walk", 4);
-        character.anims.play("character-walk-left");
+        this.character.anims.play("character-walk-left");
+
+        this.currentDirection = "down";
+        this.keyEventMap = {};
+        const keyCodes = {
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+        };
+
+        for (let direction in keyCodes) {
+            let keyEvent = this.input.keyboard.addKey(keyCodes[direction], true, true);
+            keyEvent.on("up", (e) => {
+                this.currentDirection = `${direction}`;
+            });
+            this.keyEventMap[`${direction}`] = keyEvent;
+        }
     }
 
     createAnimation(spriteName, action, numberOfFrames = 1, suffix = ".png") {
@@ -38,6 +48,25 @@ export default class Game extends Phaser.Scene {
                 repeat: -1,
                 frameRate: 5,
             });
+        }
+    }
+
+    update() {
+        if (this.keyEventMap.up.isDown) {
+            this.character.setVelocity(0, -100);
+            this.character.anims.play("character-walk-up", true);
+        } else if (this.keyEventMap.down.isDown) {
+            this.character.setVelocity(0, 100);
+            this.character.anims.play("character-walk-down", true);
+        } else if (this.keyEventMap.left.isDown) {
+            this.character.setVelocity(-100, 0);
+            this.character.anims.play("character-walk-left", true);
+        } else if (this.keyEventMap.right.isDown) {
+            this.character.setVelocity(100, 0);
+            this.character.anims.play("character-walk-right", true);
+        } else {
+            this.character.setVelocity(0, 0);
+            this.character.anims.play(`character-idle-${this.currentDirection}`, true);
         }
     }
 }
